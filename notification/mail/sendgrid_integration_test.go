9b2760ee7ed3520ef690a843d6115bf9d8b5519a
Johannes Hoenger <johannes.hoenger@gmail.com>
2021-10-20 19:19:50 +0200

    separate integration tests

---
+++ notification/mail/sendgrid_integration_test.go
@@ -0,0 +1,49 @@
+package mail
+
+import (
+	"testing"
+)
+
+const APIKey = ""
+const From = ""
+const To = ""
+
+func TestSendMail(t *testing.T) {
+	if len(APIKey) == 0 || len(From) == 0 || len(To) == 0 {
+		t.Skip("Skipping test because integration test variables are not provided.")
+	}
+
+	config := SendGridConfig{
+		APIKey:        APIKey,
+		OriginAddress: From,
+		OriginName:    "Tester",
+	}
+
+	sender := NewSendGridService(&config)
+	attributes := MailAttributes{
+		To:      []string{To},
+		Subject: "Hi",
+		Content: "Another test mail app",
+	}
+
+	err := sender.SendMail(attributes)
+	checkError(err, t)
+
+	if sender == nil {
+		t.Errorf("Sendgrid not initialized")
+	}
+}
+
+func getTestConfig() SendGridConfig {
+	return SendGridConfig{
+		APIKey:        "testkey",
+		OriginAddress: "keyaddress",
+		OriginName:    "testname",
+	}
+}
+
+func checkError(err error, t *testing.T) {
+	if err != nil {
+		t.Error(err)
+	}
+}
