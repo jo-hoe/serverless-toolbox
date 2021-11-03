4d6e891b53d39d006f5a2178825e0c650603c9e4
Johannes Hoenger <johannes.hoenger@gmail.com>
2021-11-03 21:21:13 +0100

    added cache to config provider

--- configuration/repoconfigmanager.go
+++ configuration/repoconfigmanager.go
@@ -2,33 +2,70 @@ package configuration
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/jo-hoe/serverless-toolbox/repository"
 )
 
-// RepositoryConfigProvider retrieves configurations from enviroment variables
+// RepositoryConfigProvider retrieves configurations from repository.
+// Has an interal cache to redurce calls to repositories
 type RepositoryConfigProvider struct {
-	repo repository.KeyValueRepo
+	mutex sync.RWMutex
+	repo  repository.KeyValueRepo
+	cache map[string]interface{}
 }
 
 func NewRepositoryConfigProvider(repo repository.KeyValueRepo) *RepositoryConfigProvider {
 	return &RepositoryConfigProvider{
-		repo: repo,
+		repo:  repo,
+		cache: nil,
 	}
 }
 
-// GetConfig returns a configuration for a given key. Otherwise nil is returned with an error
+// GetConfig retrieves all configs and read them into cache. If the cache is already initilized
+// the value is directly retrieved from it and returned.
 func (repositoryConfigProvider *RepositoryConfigProvider) GetConfig(configKey string) (interface{}, error) {
-	result, err := repositoryConfigProvider.repo.Find(configKey)
-	if err == nil {
-		return result.Value, err
+	repositoryConfigProvider.mutex.RLock()
+	defer repositoryConfigProvider.mutex.RUnlock()
+	// init cache
+	if repositoryConfigProvider.cache == nil {
+		repositoryConfigProvider.cache = make(map[string]interface{})
+		// load all items into cache
+		keyValuePairs, err := repositoryConfigProvider.repo.FindAll()
+		if err != nil {
+			return nil, err
+		}
+		repositoryConfigProvider.cache = make(map[string]interface{})
+		for _, keyValuePair := range keyValuePairs {
+			repositoryConfigProvider.cache[keyValuePair.Key] = keyValuePair.Value
+		}
+	}
+
+	if value, ok := repositoryConfigProvider.cache[configKey]; ok {
+		return value, nil
 	} else {
-		return nil, fmt.Errorf("could not find configuration for key '%s' error: '%v'", configKey, err)
+		return nil, fmt.Errorf("could not find configuration for key '%s' but found %+v", configKey, repositoryConfigProvider.cache)
 	}
 }
 
-// SetConfig stores a config in string form. Function overwrites existing values
+// SetConfig stores a config in string form. Function overwrites existing values.
+// The function also updates the cache.
 func (repositoryConfigProvider *RepositoryConfigProvider) SetConfig(configKey string, configValue interface{}) error {
+	repositoryConfigProvider.mutex.RLock()
+	defer repositoryConfigProvider.mutex.RUnlock()
+
 	_, err := repositoryConfigProvider.repo.Overwrite(configKey, configValue)
+	// if no error was found, item is put into the cache
+	if err != nil {
+		repositoryConfigProvider.cache[configKey] = configValue
+	}
 	return err
+}
+
+// Resets the internal cache
+func (repositoryConfigProvider *RepositoryConfigProvider) ResetCache() {
+	repositoryConfigProvider.mutex.RLock()
+	defer repositoryConfigProvider.mutex.RUnlock()
+
+	repositoryConfigProvider.cache = nil
 }
